e421599c7c42b7b658f91e10e1bb9e2fbd1050e7
gavvala-tech <85405435+gavvala-tech@users.noreply.github.com>
2021-09-13 13:07:18 +0200

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,20 @@
+public class Demo {
+    private int id;
+    private String name;
+ 
+    public int getID() {
+        return this.id;
+    }
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getName() {
+        return this.name;
+    }
+ 
+    public void setName(string name) {
+        this.name = name;
+    }
+}
